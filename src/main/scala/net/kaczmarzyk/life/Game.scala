f4157cb9ffd2da862c12d7d19b3ba18640dc468a
tkaczmarzyk <tomasz+github@kaczmarzyk.net>
2013-07-16 16:41:15 +0200

    first test: initializing first generation

---
+++ src/main/scala/net/kaczmarzyk/life/Game.scala
@@ -0,0 +1,15 @@
+package net.kaczmarzyk.life
+
+object Game {
+  
+  def apply(initialGen: String) = {
+    new Game()
+  }
+}
+
+class Game {
+
+  def generation(seq: Int) = {
+    'todo
+  }
+}
---
+++ src/test/scala/net/kaczmarzyk/life/GameTest.scala
@@ -0,0 +1,25 @@
+package net.kaczmarzyk.life
+
+import org.scalatest.FunSuite
+import org.scalatest.matchers.ShouldMatchers
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+
+@RunWith(classOf[JUnitRunner])
+class GameTest extends FunSuite with ShouldMatchers {
+
+  ignore("game initializes the first generation") {
+    val game = Game("""
+    	ooo
+    	oxx
+        ooo""")
+        
+    val gen = game.generation(0);
+    
+    gen(0, 0) should be === 'dead
+    gen(1, 0) should be === 'dead
+    gen(1, 1) should be === 'alive
+    gen(2, 1) should be === 'alive
+  }
+}
